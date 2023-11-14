a26986887937ab54aa4c69905cbd87af7eec22dd
Bishwajitkuat <bishwajitkuat@gmail.com>
2023-11-15 00:00:44 +0200

    Head and Navbar component is imported  and used

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Navbar from "@/components/navbar/navbar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello from next</h1>
+      <Head>
+        <title>Bisso | Full-Stuck Developer</title>
+        <meta
+          name="description"
+          content="portfolio of Bishwajit Das, app creaded with next js, typescript react "
+        />
+        <link rel="icon" href="" />
+      </Head>
+      <Navbar />
     </main>
   );
 }
