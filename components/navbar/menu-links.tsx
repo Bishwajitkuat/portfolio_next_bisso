a364f8231201ac15c10b7bb657e03aae7811cafa
Bishwajitkuat <bishwajitkuat@gmail.com>
2023-11-15 00:53:07 +0200

    href value added to links

--- components/navbar/menu-links.tsx
+++ components/navbar/menu-links.tsx
@@ -10,19 +10,19 @@ export default function MenuLinks({
 }) {
   return (
     <ul className={ulClass}>
-      <Link href="">
+      <Link href="/">
         <li className={liClass}>Home</li>
       </Link>
-      <Link href="">
+      <Link href="/about">
         <li className={liClass}>About</li>
       </Link>
-      <Link href="">
+      <Link href="/skills">
         <li className={liClass}>Skills</li>
       </Link>
-      <Link href="">
+      <Link href="/projects">
         <li className={liClass}>Projects</li>
       </Link>
-      <Link href="">
+      <Link href="/contact">
         <li className={liClass}>Contact</li>
       </Link>
     </ul>
