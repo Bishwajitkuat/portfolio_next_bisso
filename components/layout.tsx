c103af8d126ffbdbc77c6dde1ed8a4a2f0267ce2
Bishwajitkuat <bishwajitkuat@gmail.com>
2023-11-15 14:38:31 +0200

    Layour component is created and it is wrapping Component in _app.tsx. So, components which are part of each page will be added to Layout component and dynamic pages will be passed as children props in Layout component

---
+++ components/layout.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Navbar from "@/components/navbar/navbar";
+import HomePage from "@/components/home-page/home-page";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Head>
+        <title>Bisso | Full-Stuck Developer</title>
+        <meta
+          name="description"
+          content="portfolio of Bishwajit Das, app creaded with next js, typescript and react "
+        />
+        <link rel="icon" href="" />
+      </Head>
+      <main className="grid-1">
+        <Navbar />
+        {children}
+      </main>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,5 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Head from "next/head";
-import Navbar from "@/components/navbar/navbar";
 import HomePage from "@/components/home-page/home-page";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Bisso | Full-Stuck Developer</title>
-        <meta
-          name="description"
-          content="portfolio of Bishwajit Das, app creaded with next js, typescript react "
-        />
-        <link rel="icon" href="" />
-      </Head>
-      <main className="grid">
-        <Navbar />
-
-        <HomePage />
-      </main>
-    </>
-  );
+  return <HomePage />;
 }
