5b5dbb4c54f377103eeaaa3f74365225b7c70556
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-02-07 16:22:13 +0200

    Product card's styling is improved.

--- components/projects-page/project-card.tsx
+++ components/projects-page/project-card.tsx
@@ -20,25 +20,9 @@ export default function ProjectCard({ project }: ProjectCardPros) {
     liveSite,
   } = project;
   return (
-    <li className=" flex flex-col items-center shadow-md shadow-[#64748b]  rounded-md bg-gradient-to-br from-[#94a3b8]  to-[#e5e7eb] gap-4   ">
-      <h2 className="py-4 text-center text-2xl uppercase tracking-widest">
-        {projectName}
-      </h2>
-      <div className="flex px-2 justify-end  gap-2">
-        {skillIconUrl.map((icon, index) => (
-          <Image
-            key={index}
-            src={icon.imgPath}
-            alt={icon.imgAlt}
-            width="0"
-            height="0"
-            sizes="100vw"
-            style={{ width: "6%", height: "auto" }}
-          />
-        ))}
-      </div>
+    <li className="overflow-hidden flex flex-col items-center shadow-md shadow-[#64748b]  rounded-xl bg-gradient-to-br from-[#94a3b8]  to-[#e5e7eb] gap-4   ">
       <Image
-        className="w-full self-center shadow-sm shadow-[#64748b]"
+        className="rounded-t-xl hover:scale-105  ease-in duration-300 self-center shadow-sm shadow-[#64748b]"
         src={imgURL}
         alt={imgAlt}
         width="0"
@@ -48,17 +32,53 @@ export default function ProjectCard({ project }: ProjectCardPros) {
         priority={false}
       />
 
-      <p className="grow px-2">{projectDescription}</p>
-      <ul className=" pb-4 flex gap-6">
-        {liveSite && (
-          <Link href={liveSite} target="_blank">
-            <TbWorldWww size={40} color="#4540ed" />
+      <div className="w-full px-4 flex justify-between gap-4">
+        <ul className="flex flex-col gap-2 ">
+          {liveSite && (
+            <Link
+              href={liveSite}
+              target="_blank"
+              className="flex gap-1 items-center hover:scale-110 ease-in duration-100"
+            >
+              <div>
+                <TbWorldWww size={24} color="#1e3a8a" />
+              </div>
+              <p className="text-[#1e3a8a] text-md font-semibold tracking-widest">
+                Live
+              </p>
+            </Link>
+          )}
+          <Link
+            href={gitHub}
+            target="_blank"
+            className="flex gap-1 items-center hover:scale-105 ease-in duration-200"
+          >
+            <div>
+              <FaGithub color="#1e3a8a" size={24} />
+            </div>
+            <p className="text-md text-blue-900 font-semibold tracking-widest">
+              GitHub
+            </p>
           </Link>
-        )}
-        <Link href={gitHub} target="_blank">
-          <FaGithub color="#4540ed" size={40} />
-        </Link>
-      </ul>
+        </ul>
+        <div className="flex items-start justify-end flex-wrap gap-2">
+          {skillIconUrl.map((icon, index) => (
+            <Image
+              key={index}
+              src={icon.imgPath}
+              alt={icon.imgAlt}
+              width="0"
+              height="0"
+              sizes="100vw"
+              style={{ width: "2rem", height: "auto" }}
+            />
+          ))}
+        </div>
+      </div>
+      <h2 className="py-8 px-4 text-center text-2xl uppercase tracking-widest">
+        {projectName}
+      </h2>
+      <p className="grow px-4 pb-4">{projectDescription}</p>
     </li>
   );
 }
