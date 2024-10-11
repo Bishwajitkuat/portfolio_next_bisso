68fe1fa2a260d99d362b50ce350c99a7fd949137
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-10-11 17:49:32 +0300

    Update skill section

--- components/skills-page/skills-page.tsx
+++ components/skills-page/skills-page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {
   programmingLanguages,
-  fullStackTechs,
   frontEndTechs,
   backEndTechs,
   devOpsTechs,
@@ -36,27 +35,7 @@ export default function SkillsPage() {
             </div>
           </div>
         </div>
-        <div className="py-8">
-          <div className="bg-gradient-to-br from-blue-500 via-red-500 to-green-500 rounded-xl p-[1px] shadow-[10px_0_60px_-15px_rgba(0,0,0,0.3)] ">
-            <div className="bg-[#cbd5e1] rounded-xl  ">
-              <h2 className="  text-center text-2xl uppercase tracking-widest py-8">
-                Full-Stack Technologies
-              </h2>
-              <ul className="py-8 px-4 grid grid-cols-2 md:grid-cols-6  gap-6 items-end">
-                {fullStackTechs.map((skill) => (
-                  <SkillCard
-                    key={skill.id}
-                    imgFileName={skill.imgFileName}
-                    imgAlt={skill.imgAlt}
-                    skillName={skill.skillName}
-                    skillLevel={skill.skillLevel}
-                    link={skill.link}
-                  />
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
+
         <div className="py-8">
           <div className="bg-gradient-to-br from-blue-500 via-red-500 to-green-500 rounded-xl p-[1px] shadow-[10px_0_60px_-15px_rgba(0,0,0,0.3)] ">
             <div className="bg-[#cbd5e1] rounded-xl  ">
